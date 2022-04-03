0e7674b4c3c238490284761ddef3c4354d9df01a
Matt Robinson <matthewdrobinson@gmail.com>
2022-04-03 16:44:10 +0200

    fixed Navlinks

--- src/navBar/navBar.js
+++ src/navBar/navBar.js
@@ -12,22 +12,22 @@ export default function NavBar() {
     <div class="navcolumns">
 
       <div class="navcolumn">
-        <a href="/"><img src={homeIcon} id="nav-image" alt="About" /></a>
+        <NavLink to="/"><img src={homeIcon} id="nav-image" alt="About" /></NavLink>
         <NavLink to="/" className="nav-link" activeStyle={{ color: 'white' }}> About </NavLink>
         </div>
 
       <div class="navcolumn">
-        <a href="/expertise"><img src={aboutIcon} id="nav-image" alt="Expertise" /></a>
+        <NavLink to="/expertise"><img src={aboutIcon} id="nav-image" alt="Expertise" /></NavLink>
         <NavLink to="/expertise" className="nav-link">Expertise</NavLink>
         </div>
 
       <div class="navcolumn">
-        <a href="/experience"><img src={portIcon} id="nav-image" alt="Experience" /></a>
+        <NavLink to="/experience"><img src={portIcon} id="nav-image" alt="Experience" /></NavLink>
         <NavLink to="/experience" className="nav-link">Experience</NavLink>
         </div>
 
       <div class="navcolumn">
-        <a href="/contact"><img src={contIcon} id="nav-image" alt="Contact" /></a>
+        <NavLink to="/contact"><img src={contIcon} id="nav-image" alt="Contact" /></NavLink>
         <NavLink to="/contact" className="nav-link">Contact</NavLink>
         </div>
         
